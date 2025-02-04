d594cd76be5e1ad68ba522c02b4f6dabe4134d63
SAURABH <er.saurabhkumar22@gmail.com>
2025-02-04 19:44:53 +0530

    04-Feb

--- src/test/java/com/automationdummyprojects/Dummy_projects_Locators/Dummy_1_Awesomeqa.java
+++ src/test/java/com/automationdummyprojects/Dummy_projects_Locators/Dummy_1_Awesomeqa.java
@@ -1,4 +1,50 @@
 package com.automationdummyprojects.Dummy_projects_Locators;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.Select;
+
 public class Dummy_1_Awesomeqa {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        EdgeDriver driver = new EdgeDriver();
+        driver.get("https://awesomeqa.com/practice.html");
+        driver.manage().window().maximize();
+        Thread.sleep(5000);
+
+        //Text box
+        driver.findElement(By.xpath("//input[@name = 'firstname']")).sendKeys("SAURABH");
+        driver.findElement(By.xpath("//input[@name = 'lastname']")).sendKeys("SRIVASTAVA");
+
+        //Radio button
+        driver.findElement(By.xpath("//input[contains(@id,'sex-0')]")).click();
+        driver.findElement(By.xpath("//input[contains(@id,'exp-4')]")).click();
+
+        //text box for date
+        driver.findElement(By.xpath("//input[contains(@id, 'datepicker')]")).sendKeys("29-Jan-2025");
+
+        //checkbox
+        driver.findElement(By.xpath("//input[contains(@value,'Automation Tester')]")).click();
+        driver.findElement(By.xpath("//input[contains(@value,  'Manual Tester')]")).click();
+        driver.findElement(By.xpath("//input[contains(@value,'Selenium Webdriver')]")).click();
+
+        //Selct dropdown by selct method
+        Select continents = new Select(driver.findElement(By.id("continents")));
+        //continents.selectByVisibleText("Australia");
+        continents.selectByIndex(3);
+
+        //Select value by scrooll bropdown
+        Select selenium_commands = new Select(driver.findElement(By.id("selenium_commands")));
+        selenium_commands.selectByIndex(3);
+        selenium_commands.selectByContainsVisibleText("Switch Commands");
+
+        //upload file button
+        //driver.findElement(By.xpath("//input[contains(@id,'photo')]")).click();
+        WebElement upload = driver.findElement(By.xpath("//input[contains(@type,  'file')]"));
+        upload.sendKeys("C:\\Users\\SAURABH\\Downloads\\zoomsample");
+        System.out.println("File uploaded successfully");
+
+    }
 }
---
+++ src/test/java/com/automationdummyprojects/Dummy_projects_Locators/Dummy_2_CuraHealthApp.java
@@ -0,0 +1,8 @@
+package com.automationdummyprojects.Dummy_projects_Locators;
+
+public class Dummy_2_CuraHealthApp {
+
+    public static void main(String[] args) {
+
+    }
+}
