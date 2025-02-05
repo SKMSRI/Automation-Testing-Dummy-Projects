05854fc910a94920befaffa8cef5247f9ac2de62
SAURABH <er.saurabhkumar22@gmail.com>
2025-02-05 20:10:59 +0530

    04-Feb

--- src/test/java/com/automationdummyprojects/Dummy_projects_Locators/Dummy_2_CuraHealthApp.java
+++ src/test/java/com/automationdummyprojects/Dummy_projects_Locators/Dummy_2_CuraHealthApp.java
@@ -1,8 +1,27 @@
 package com.automationdummyprojects.Dummy_projects_Locators;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.edge.EdgeDriver;
+
 public class Dummy_2_CuraHealthApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        EdgeDriver driver =new EdgeDriver();
+        driver.get(" https://katalon-demo-cura.herokuapp.com");
+        driver.findElement(By.id("btn-make-appointment")).click();
+        Thread.sleep(5000);
+
+        //Login
+        driver.findElement(By.id("txt-username")).sendKeys("John Doe");
+        driver.findElement(By.id("txt-password")).sendKeys("ThisIsNotAPassword");
+        driver.findElement(By.id("btn-login")).click();
+        System.out.println(driver.getCurrentUrl());
+        //Action events
+
+
+
+
 
     }
 }
