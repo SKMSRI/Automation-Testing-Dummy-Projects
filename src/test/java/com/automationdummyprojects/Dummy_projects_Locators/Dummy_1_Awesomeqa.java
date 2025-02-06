3f5257e711c7a845c0b58fc9abc8f4d5c33af7e9
SAURABH <er.saurabhkumar22@gmail.com>
2025-02-06 19:11:38 +0530

    04-Feb

--- src/test/java/com/automationdummyprojects/Dummy_projects_Locators/Dummy_1_Awesomeqa.java
+++ src/test/java/com/automationdummyprojects/Dummy_projects_Locators/Dummy_1_Awesomeqa.java
@@ -6,13 +6,12 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class Dummy_1_Awesomeqa {
-
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
         EdgeDriver driver = new EdgeDriver();
         driver.get("https://awesomeqa.com/practice.html");
         driver.manage().window().maximize();
-        Thread.sleep(5000);
+        //Thread.sleep(5000);
 
         //Text box
         driver.findElement(By.xpath("//input[@name = 'firstname']")).sendKeys("SAURABH");
@@ -48,3 +47,5 @@ public class Dummy_1_Awesomeqa {
 
     }
 }
+
+
